7e2ea5c1b4c2de1011b03eab98e57d8a4b4dde4b
Philip J. Reed <philip.reed@anthem.com>
2020-02-02 19:05:54 -0500

    Parse out the history.

--- petersburg/petersburg_analyzer.py
+++ petersburg/petersburg_analyzer.py
@@ -1,4 +1,6 @@
+import re
 import requests
+
 class PetersburgAnalyzer(object):
     def __init__(self, game_id=None, html=None, filename=None):
         self._game_id = game_id
@@ -8,14 +10,35 @@ class PetersburgAnalyzer(object):
             self._html = self.game_history_from_file(filename)
         else:
             self._html = html
+        self._history = []
 
     def diag(self):
-        print(self._html)
+        self.init_history_from_html()
+        print(self._history)
 
     def html_for_id(self, game_id):
         url = "https://yucata.de/en/Game/Petersburg/{}".format(game_id)
         response = requests.get(url)
         return response.text
+
+    def init_history_from_html(self):
+        for line in self._html.split(';'):
+            # There is a catch. A few move types have three numbers; when that happens,
+            # we need to make the comma optional but drop it in the group
+            m = re.match("\nHistoryMove.([0-9]*). = '([0-9]*),([0-9]*),?([0-9]*)?'", line)
+            if m:
+                assert len(self._history) == int(m.group(1)), "exp.: " + str(m.group(1))
+                try:
+                    tup = (int(m.group(2)), int(m.group(3)), int(m.group(4)))
+                except ValueError:
+                    tup = (int(m.group(2)), int(m.group(3)))
+                self._history.append({'move': tup})
+                continue
+
+            m = re.match("\nHistoryStatus.([0-9]*). = '([0-9A-Z]*)'", line)
+            if m:
+                assert len(self._history) == int(m.group(1)) + 1
+                self._history[-1]['status'] = str(m.group(2))
 
     @staticmethod
     def game_history_from_file(fn):
