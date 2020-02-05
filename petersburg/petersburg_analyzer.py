6b7fd2a7a0fee463d50d100047e2c96b01774d08
Philip J. Reed <philip.reed@anthem.com>
2020-02-04 20:01:23 -0600

    Extracted out a PetersburgHistory class.

--- petersburg/petersburg_analyzer.py
+++ petersburg/petersburg_analyzer.py
@@ -2,49 +2,34 @@ import pprint
 import re
 import requests
 
-class PetersburgAnalyzer(object):
-    def __init__(self, game_id=None, html=None, filename=None):
-        self._game_id = game_id
-        if html is not None:
-            self._html = self.html_for_id(game_id)
-        elif filename is not None:
-            self._html = self.game_history_from_file(filename)
-        else:
-            self._html = html
-        self._history = []
+class PetersburgHistory(object):
+    def __init__(self, html):
+        self._html = html
+        self._moves = []
+        self.init_from_html()
 
     def diag(self):
-        pprint.pprint([h['move_str'] for h in self._history])
+        pprint.pprint([m['move_str'] for m in self._moves])
 
-    def html_for_id(self, game_id):
-        url = "https://yucata.de/en/Game/Petersburg/{}".format(game_id)
-        response = requests.get(url)
-        return response.text
-
-    def init_history_from_html(self):
+    def init_from_html(self):
         for line in self._html.split(';'):
             # There is a catch. A few move types have three numbers; when that happens,
             # we need to make the comma optional but drop it in the group
             m = re.match("\nHistoryMove.([0-9]*). = '([0-9]*),([0-9]*),?([0-9]*)?'", line)
             if m:
-                assert len(self._history) == int(m.group(1)), "exp.: " + str(m.group(1))
+                assert len(self._moves) == int(m.group(1)), "exp.: " + str(m.group(1))
                 try:
                     tup = (int(m.group(2)), int(m.group(3)), int(m.group(4)))
                 except ValueError:
                     tup = (int(m.group(2)), int(m.group(3)))
-                self._history.append({'move': tup})
-                self._history[-1]['move_str'] = self.move_str(tup)
+                self._moves.append({'move': tup})
+                self._moves[-1]['move_str'] = self.move_str(tup)
                 continue
 
             m = re.match("\nHistoryStatus.([0-9]*). = '([0-9A-Z]*)'", line)
             if m:
-                assert len(self._history) == int(m.group(1)) + 1
-                self._history[-1]['status'] = str(m.group(2))
-
-    @staticmethod
-    def game_history_from_file(fn):
-        with open(fn, 'r') as f:
-            return f.read()
+                assert len(self._moves) == int(m.group(1)) + 1
+                self._moves[-1]['status'] = str(m.group(2))
 
     @staticmethod
     def cards_dict():
@@ -113,9 +98,34 @@ class PetersburgAnalyzer(object):
             object = cards[tup[1]]
         return "{} {}".format(verbs[tup[0]], object)
 
+    def break_into_rounds(self):
+        pass
+
+class PetersburgAnalyzer(object):
+    def __init__(self, game_id=None, html=None, filename=None):
+        self._game_id = game_id
+        if html is None and game_id is not None:
+            html = self.html_for_id(game_id)
+        elif html is None and filename is not None:
+            html = self.game_history_from_file(filename)
+        self._history = PetersburgHistory(html)
+
+    def diag(self):
+        self._history.diag()
+
+    def html_for_id(self, game_id):
+        url = "https://yucata.de/en/Game/Petersburg/{}".format(game_id)
+        response = requests.get(url)
+        return response.text
+
+    @staticmethod
+    def game_history_from_file(fn):
+        with open(fn, 'r') as f:
+            return f.read()
+
+
 def main():
     a = PetersburgAnalyzer(filename='petersburg/tests/inputs/ph_9514605.js')
-    a.init_history_from_html()
     a.diag()
 
 def test_move_str():
