42ba756e0a0213a4258dabb4d6b3bed06f307336
Philip J. Reed <philip.reed@anthem.com>
2020-02-16 13:18:19 -0500

    Misc - different URL handling and note on Selenium

---
+++ notes.md
@@ -0,0 +1,5 @@
+I started to install Selenium.
+[link](https://saucelabs.com/resources/articles/getting-started-with-webdriver-in-python-on-osx)
+
+Pretty sure I got as far as Chromedriver, and indeed I see it on my system. 
+However I may not even need it. See https://stackoverflow.com/questions/1820927/request-monitoring-in-chrome
--- site-yucata/classify_games.py
+++ site-yucata/classify_games.py
@@ -17,7 +17,7 @@ class GamesFinder(object):
         self._game = game
 
     def request_top_players(self):
-        url = 'https://yucata.de/de/GameInfo/' + self.
+        url = 'https://yucata.de/de/GameInfo/' + self._game
         # Better to use https://yucata.de/en/Ranking/Game/<game>
         try:
             response = requests.get(url)
@@ -29,29 +29,59 @@ class GamesFinder(object):
         patt = 'User/([a-zA-Z0-9 ]*)"'
         return [re.search(patt, l).group(1) for l in lines if re.search(patt, l)]
 
+    @staticmethod
+    def user_id_for(name):
+        url = 'https://yucata.de/de/Ranking/' + name
+        text = requests.get(url).text
+        try:
+            # return re.search('UpdatePlayerRankingList\(([0-9]*)\)', text).group(1)
+            return re.search('UpdatePlayerRanking', text).group(1)
+        except:
+            ## This might happen legitimately if the player is not found.
+            # In our case, starting up, it's more likely a bad regex
+            raise
+
+    # Here's an example of the Ajax URL that I see by using developer tools.
+    # This is for hotei11, a top Petersburg player.
+    # https://yucata.de/data.jqdt?dataSource=RankingDetailsUser&UserID=188172&OppID=-1&
+    #   GameType=-1&draw=1&columns%5B0%5D%5Bdata%5D=function&columns%5B0%5D%5Bname%5D=&
+    #   columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false
+    #   &columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&
+    #   columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&
+    #   columns%5B1%5D%5Bdata%5D=FinishedOnString&columns%5B1%5D%5Bname%5D=&
+    #   columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=CustomGameName&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=Opponents&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=GameId&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=PointResult&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=25&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1581302442930
+
+    # Apparently nothing after GameType is necessary.
+
+    # Originally get the ID by parsing: var profileId = 188172;  or var CurrentUserId = 188172;
+    # Maybe different though? Rashtulla 339070 found by that number in the JS.
     def request_games_for(self, player):
-        url = 'https://yucata.de/de/Ranking/' + player
+        id = self.user_id_for(player)
+        url = "https://yucata.de/data.jqdt?dataSource=RankingDetailsUser&UserID=" + id
         try:
             response = requests.get(url)
         except TimeoutError as e:
             raise YucataOSError(wrapped_error=e, game=self._game, state='for player')
+        return response.text
 
     def find(self):
-        #print("NYI game={}".format(self._game))
         top = self.request_top_players()
-        print(top)
+        for player in top:
+            text = self.request_games_for(player)
+            print(text)
+
 
 def main():
-    # GamesFinder('Snowdonia').find()
-    # GamesFinder('Petersburg').find()
-    # GamesFinder('Delphi').find()
+    # GamesFinder('Snowdonia').find()       # ID 309
+    GamesFinder('Petersburg').find()      # 27
+    # GamesFinder('Delphi').find()          # 324
     # ['JimF', 'Uli', 'JayAnthony', 'kyoysc', 'LJenkins', 'mcdoomer', 'suet63', 'Minella',
     #  'BlauerKlaus', 'sto1']
     # ['hotei11', 'thekid', 'c4dancer', 'CroOm', 'texaswer', 'oelschlegel', 'Monash',
     #  'Ostrich', 'Chrissi', 'georgedim']
     # ['Stonecrusher', 'kyoysc', 'Hircine', 'peet', 'Pompkin', 'Tintin', 'November',
     #  'Alanna', 'vospe', 'Katzenfloh']
-    # GamesFinder('RRR2').find()
+    # GamesFinder('RRR2').find()            # 142
     # ['califax', 'Levster', 'tamihiko71b', 'Agentus', 'loulouulou', 'Gio1987', 'RobRdam',
     #  'oelschlegel', 'noblekenny', 'AndreasJ']
 
