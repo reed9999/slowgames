6933255a24471e27b0ce7b084a67726f05959b38
Philip J. Reed <philip.reed@anthem.com>
2020-02-26 23:24:39 -0500

    Continuous process on Few Acres standalone. Still some clearly wrong cards, and there will be many that are quite hard to dig out.

--- few-acres-of-snow/few_acres_adhoc.py
+++ few-acres-of-snow/few_acres_adhoc.py
@@ -1,19 +1,27 @@
 """I failed to commit changes from another computer. This file contains functions that
-should be turned into methods."""
+should be turned into methods.
 
-UK_CARDS = {
-    None: 'Boston',
+Use this link for all philip9999's AFAoS games (player ID: 404792):
+https://yucata.de/data.jqdt?dataSource=RankingDetailsUser&UserID=404792&GameType=90&length=-1
+or just use the UI from here (probably easier):
+https://yucata.de/en/Ranking/philip9999
+"""
+
+import pprint
+
+UK_CARDS = {    #  å·»
+    '°': 'Boston',
     '±': 'New Haven',
-    None: 'New York',
-    None: 'Norfolk',
-    None: 'Pemaquid',
-    None: 'Philadelphia',
-    None: "St. Mary's",
-    None: 'Albany',
+    '²': 'New York',
+    '³': 'Norfolk',
+    '´': 'Pemaquid',
+    'µ': 'Philadelphia',
+    '¶': "St. Mary's",
+    '·': 'Albany',
     None: 'Baltimore',
-    None: 'Canso',
+    '¹': 'Canso',
     None: 'Cumberland',
-    None: 'Deerfield',
+    '»': 'Deerfield',
     None: 'Detroit',
     None: 'Fort Beausejour',
     None: 'Fort Duquesne',
@@ -24,23 +32,39 @@ UK_CARDS = {
     None: 'Fort St. John',
     None: 'Fort Stanwix',
     None: 'Fort Venango',
-    None: 'Fort William Henry',
+    'Æ': 'Fort William Henry',
     None: 'Gaspe',
-    None: 'Halifax',
+    'È': 'Halifax',
     None: 'Kennebec',
     None: 'Louisbourg',
     None: 'Oswego',
-    None: 'Port Royal',
-    None: 'Richmond',
-    None: 'Tadoussac',
+    'Ì': 'Port Royal',
+    'Í': 'Richmond',
+    'Î': 'Tadoussac',
     None: 'Ticonderoga',
     None: 'Trois Rivieres',
     None: 'Michillimackinac',
 
+    'Ó': 'Regular infantry',
+    'Õ': 'Regular infantry',
+    'Ö': 'Regular infantry',
+    'Ò': 'Regular infantry',
     'ã': 'Rangers',
 
-    # ° Gaspé
-    # ² Montréal
+    'C': 'Native Americans',
+    'D': 'Native Americans',
+    'E': 'Native Americans',
+    'F': 'Native Americans',
+    'G': 'Native Americans',
+    'H': 'Settlers',
+
+
+    'Ø': 'Indian leader',
+    'Ñ': 'Military leader',
+    'Ý': 'Militia',
+    'å': 'Governor',
+    'ä': 'Fortification (red)',
+
 }
 
 FR_CARDS = {
@@ -60,7 +84,7 @@ FR_CARDS = {
     None: 'Fort Frontenac',
     None: 'Fort Halifax',
     None: 'Fort Niagara',
-    None: 'Fort Presqu\'île',
+    '¿': "Fort Presqu'île",
     None: 'Fort St. John',
     None: 'Fort Stanwix',
     None: 'Fort Venango',
@@ -69,8 +93,10 @@ FR_CARDS = {
     None: 'Halifax',
     None: 'Kennebec',
     '±': 'Louisbourg',
-    None: 'Oswego',
-    '´': 'Port Royal',
+    'Ð': 'Oswego (dead)',
+    # '´': 'Port Royal',
+    'Ì': 'Port Royal',
+    'Ç': 'Port Royal (dead maybe)',
     None: 'Richmond',
     'µ': 'Tadoussac',
     None: 'Ticonderoga',
@@ -80,16 +106,38 @@ FR_CARDS = {
     None: 'Michillimackinac',
 
     'Ë': 'RegInf (free)',
-    'Ì': 'Trader',
+    'Ù': 'Regular infantry',
+    'Ú': 'Regular infantry',
+    'Û': 'Regular infantry',
+    'Ò': 'Military leader',
+
+    'Ó': 'Militia',
+    'Ö': 'Native Americans (blue)',
+    # 'Ì': 'Trader',
+    None: 'Trader',
+    'Í': 'Coureurs de Bois',
+    'Ñ': 'Intendant',
+    'Î': 'Fortification (blue)',
+    '´': 'Home support',
+    'Ê': 'Bateaux (huh?)',
+
 }
 
 ACTIONS = {
     '¹': 'money from',
+    '²': 'fortify (UK) or intendant (FR)',     #UK at least
     '½': 'draft',
     '¼': 'piracy',
     '»': 'trade',
     '¾': 'discard',
     '¿': 'put into reserve',
+    'À': 'retrieve reserve',
+    'º': 'merchant',
+    '°': 'settle',
+    '±': 'develop',
+    '³': 'besiege',
+    '´': 'play to siege',
+    'µ': 'raid',
 }
 
 def action_code_to_action(code, which_side):
@@ -110,9 +158,112 @@ def file_to_history(filename):
 def main():
     pass
 
+TEST_BED = [
+    # 9547143
+    'º³µ²',
+    '¼±´',
+    'º³µ°,½Ó',
+    '»Ì°²¶,½Í',
+    '°È´³¶,¹²',
+    '¿Ë,¹³',
+    '³Ì°±Ó,½Õ',
+    'ÀË,´ÌË,¾°µ¶',
+    '´ÌÕ,´Ì±',
+    '´Ì²,½Ú',
+    '´Ì²,¹µ',
+    '´Ì±,»Ì°',
+    '´ÌÈ,±´´¶',
+    '´Ì³,½Ù',
+    'º³µ°,½Ö',
+    '´ÌÚ,½Î',
+    '´ÌÖ,¹µ',
+
+    '¶DC,½Ò',
+    'Î0LÍDB,±±±²,½å',
+    'ÀÏ,ÁÏ°,ÃÐ,»Ì¶',
+    '¿Ý,¾Ê',
+    'µ´DÖRC,¹³',
+    'ÃÙ,¸ØC,µÒãE',
+    'ÃÐ,»Ì²¶,¿Ò',
+]
+
+TEST_BED_02 = [
+    '°È°³¶',
+    '¿Ë',
+    'º±²µ,½ä',
+    '¹¶,½Î',
+    '±³³¶,±´´°',
+    '¼±´,¹²',
+    '°·²¶µ,¹°',
+    '»Ì²,±Îµ³',
+    '²ÈäÈ,½H',
+    '²Ì´Î,¹¶',
+    'ºÈ³,¾´',
+    '»Ì°¶,½Í',
+    '²·ä·,°»±¶°',
+    '²Ò³Î,¼±´',
+    '±»»H,±µµ²',
+    '¼±´,»Ì²µ',
+    '±±±°,¿ä',
+    '²ÇÎ°,±Ð¶³',
+    '±ÈÈ¶,½C',
+    '²Ð¶Î,¹´',
+    '±··H,¹³',
+    '±Ç°³,»Ìµ',
+    '±¶¶²,½D',
+    '»Ì²µ,½Ó',
+    '½Ñ,½å',
+    '²ÊÎ±,º³¶',
+    'ºÈ±°,¾´',
+    'º´¶,½Ö',
+    'º³µ»,½Ò',
+    '²ÎÎµ,»Ì°',
+    '°¹È°H,ÄP',
+    '±Ê±³,¿Ó',
+    '¹²,¿å',
+    '±Ñ²³,¾°',
+    '°Æ·¶,µÑCDRÖ,Àäå',
+    '»Ìµ,ÄP',
+    'Áå±´,¿Ñ',
+    '¹¶,½Ñ',
+    'ÁåÈ,¾Ò',
+    '¾±,¾µ',
+    'µÑCDR²,¹°',        ## Raid on Montreal with two natives, failing.
+    'µ¹ÍRä,½I',
+    '°Í³¶²,¾H',
+    'Â²,²ÑÎ²',
+    '¹µ,½Ý',
+    '±Ì´³,»Ì°¶',
+    '¹²,¹»',
+    '¾I,¾Ì',
+    '¿Ý,¾·',
+    '¾Î,ÄP',
+    '±ÍÍ°,¾å',
+    '¾°,¾Ñ',
+    '±¹¹µ,¿Ò',
+    '°¿²µ,¹¶',
+]
 def test():
-    print(move_to_actions('»Ì°²¶,½Í', 'fr'))
+    players = ['uk', 'fr']
+    current = 0
+    for test in TEST_BED_02:
+        print(players[current])
+        for action in move_to_actions(test, players[current]):
+            print("-- {}".format(action))
+        current = (current + 1) % 2
 
 if __name__ == "__main__":
     main()
     test()
+
+
+## FUN EXPLORATORY CODE
+"""
+// for France
+function moo() {
+HistoryStatus[1] = 
+    '308°°²±°;²±²±±±±°°°°°°°°°°°°°°°°µ°°µ°µ°µ°µµ¶°;Ì,;°²°°--±±°°°°,12,±²´µ¶,,³,,°,,±°°°°,°°±²,;±²°°--±±°°°°,5,' + 
+    x + x + ',,¶´±,,Ë,,±°°°°,°°±²,;';
+    }
+x='¿'; moo();
+"""
