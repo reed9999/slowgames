8ba97a1f89e605566e96c55da92566d5f7ac111b
Philip J. Reed <>
2020-05-10 13:02:28 -0500

    NON-WORKING. Attempt to bring almost everything into analyzer class. Problem: Lookup tables into instance methods are hard, maybe even impossible. Next step: Try them as class methods passing self.

--- few_acres_of_snow/few_acres_adhoc.py
+++ few_acres_of_snow/few_acres_adhoc.py
@@ -26,265 +26,260 @@ from few_acres_of_snow.test_moves import moves9575653_fr
 class FewAcresOfSnowHistory(GameHistory):
     pass
 
-## These handlers need to precede the actions using them.
-def raid(predicate):
-    s = "upon {target} by {subject} {extra}".format(
-        target=predicate[0],
-        subject=predicate[1],
-        extra='<TODO: what happened?>',
-    )
+class FewAcresOfSnowAnalyzer(GameAnalyzer):
+    # Redefine to be closer to how the JavaScript works.
+    # See user-interface-notes.txt or .md for a fuller explanation.
+    this_class = FewAcresOfSnowAnalyzer
+    def __init__(self, list_of_moves):
+        self.game_type='FewAcresOfSnow'
+        self.which_side = 'uk'
+        self.move_number = 0
+        self.moves_list = list_of_moves
+        self.actions_list = []
 
-def ambush(detail_code):
-    pass
+    def raid(self, predicate):
+        s = "upon {target} by {subject} {extra}".format(
+            target=predicate[0],
+            subject=predicate[1],
+            extra='<TODO: what happened?>',
+        )
+        return s
 
-# Forward "declaration" (i.e. assignment) lets us keep all the card lists together.
-UK_CARDS, FR_CARDS = None, None
+    def ambush(self, detail_code):
+        pass
 
-# More handlers, stacked this way because of dependencies.
-def simple_cards(detail_code, which_side):
-    cards_dict = {'uk': UK_CARDS, 'fr': FR_CARDS}[which_side.lower()]
-    cards = "; ".join([cards_dict[c] if c in cards_dict.keys()
-                       else c for c in detail_code
-                       ])
-    return cards
+    def simple_cards_new(self, detail_code, ):
+        cards_list, offset = self.UK_CARDS, 33 if self.which_side == 'uk' else self.FR_CARDS, 33
+        cards = "; ".join([cards_list[ord(c) - 176 - offset]
+                           for c in detail_code])
+        return cards
 
+    def simple_cards(self, detail_code, ):
+        # This is purely because I haven't yet implemented the cards list for FR
+        if self.which_side == 'uk':
+            return self.simple_cards_new(detail_code)
+        cards_dict = self.FR_CARDS   # given that uk now has a different function.
+        cards = "; ".join([cards_dict[c] if c in cards_dict.keys()
+                           else c for c in detail_code
+                           ])
+        return cards
 
-def location_then_cards(detail_code):
-    """For reinforce siege but I'll bet there are others. First the target
-    location then cards played"""
-    rv = LOCATIONS[ord(detail_code[0]) - 176] + " -- cards:"
-    rv += simple_cards(detail_code[1:], 'fr')
-    return rv
+    @staticmethod
+    def location(char):
+        return __class__.LOCATIONS[ord(char) - 176]
 
-def win_siege(detail_code):
-    if detail_code == '0XX':
-        return "No empire cards for the losing side to relinquish"
-    return "TODO: <{}>".format(detail_code)
+    def empire(self, a_char):
+        if self.which_side == 'uk':
+            offset = 33
+            return self.UK_CARDS[ord(a_char) - 176 - offset]
+        else:
+            offset = 27
+            return self.FR_CARDS[a_char]   #for now....
 
-UK_CARDS = {
-    '°': 'Boston',
-    '±': 'New Haven',
-    '²': 'New York',
-    '³': 'Norfolk',
-    '´': 'Pemaquid',
-    'µ': 'Philadelphia',
-    '¶': "St. Mary's",
-    '·': 'Albany',
-    None: 'Baltimore',
-    '¹': 'Canso',
-    None: 'Cumberland',
-    '»': 'Deerfield',
-    None: 'Detroit',
-    None: 'Fort Beausejour',
-    None: 'Fort Duquesne',
-    None: 'Fort Frontenac',
-    'À': 'Fort Halifax',
-    None: 'Fort Niagara',
-    None: 'Fort Presqu\'île',
-    None: 'Fort St. John',
-    None: 'Fort Stanwix',
-    None: 'Fort Venango',
-    'Æ': 'Fort William Henry',
-    None: 'Gaspe',
-    'È': 'Halifax',
-    None: 'Kennebec',
-    None: 'Louisbourg',
-    None: 'Oswego',
-    'Ì': 'Port Royal',
-    'Í': 'Richmond',
-    'Î': 'Tadoussac',
-    None: 'Ticonderoga',
-    None: 'Trois Rivieres',
-    None: 'Michillimackinac',
+    def any_card(self, a_char, which_side='uk'):
+        """This may be too fancy -- can we just concat LOCATIONS and the empire cards to one list?"""
+        try:
+            return self.location(a_char)
+        except:
+            return self.empire(a_char)
 
-    'Ó': 'Regular infantry',
-    'Õ': 'Regular infantry',
-    'Ö': 'Regular infantry',
-    'Ò': 'Regular infantry',
-    'Ô': 'Regular infantry',
-    'ã': 'Rangers',
+    def location_then_cards(self, detail_code):
+        """For reinforce siege but I'll bet there are others. First the target
+        location then cards played"""
+        rv = location(detail_code[0]) + " -- cards: "
+        rv += simple_cards(detail_code[1:], 'fr')
+        return rv
 
-    'C': 'Native Americans',
-    'D': 'Native Americans',
-    'E': 'Native Americans',
-    'F': 'Native Americans',
-    'G': 'Native Americans',
-    'H': 'Settlers',
+    def besiege(self, detail_code):
+        """Besiege is notable because (like some others, perhaps Trader) all the detail is usually locations
+        except for the military card, which can be either a location or an empire card. The transport can
+        also on occasion be a an empire card. I don't know
+        how the two are distinguished."""
+        target = self.location(detail_code[0])
+        launch = self.location(detail_code[1])
+        transport = self.location(detail_code[2])
+        return "on {} from {} "
 
+    def win_siege(detail_code):
+        if detail_code == '0XX':
+            return "No empire cards for the losing side to relinquish"
+        return "TODO: figure out what this means: <{}>".format(detail_code)
 
-    'Ø': 'Indian leader',
-    'Ñ': 'Military leader',
-    'Ý': 'Militia',
-    'Þ': 'Militia',
-    'å': 'Governor',
-    'ä': 'Fortification (red)',
-
-    'Ù': 'Home Support',
-
-}
-
-FR_CARDS = {
-    None: 'New Haven',
-    None: 'Norfolk',
-    None: 'Pemaquid',
-    None: 'Philadelphia',
-    None: "St. Mary's",
-    None: 'Albany',
-    None: 'Baltimore',
-    None: 'Canso',
-    None: 'Cumberland',
-    None: 'Deerfield',
-    None: 'Detroit',
-    None: 'Fort Beausejour',
-    None: 'Fort Duquesne',
-    None: 'Fort Frontenac',
-    None: 'Fort Halifax',
-    None: 'Fort Niagara',
-    '¿': "Fort Presqu'île",
-    None: 'Fort St. John',
-    None: 'Fort Stanwix',
-    None: 'Fort Venango',
-    None: 'Fort William Henry',
-    '°': 'Gaspé',
-    None: 'Halifax',
-    None: 'Kennebec',
-    '±': 'Louisbourg',
-    None: 'Oswego',         # Ð old and suspect now HS
-    '´': 'Port Royal',
-    'Ç': 'Port Royal (sometimes?)',
-    None: 'Richmond',
-    'µ': 'Tadoussac',
-    None: 'Ticonderoga',
-    '¶': 'Trois Rivieres',
-    '²': 'Montréal',
-    '³': 'Quebec',
-    None: 'Michillimackinac',
-
-    'Ë': 'RegInf (free)',
-    'Ù': 'Regular infantry',
-    'Ú': 'Regular infantry',
-    'Û': 'Regular infantry',
-    'Ò': 'Military leader',
-
-    'Ó': 'Militia',
-    'Ö': 'Native Americans (blue)',
-    'Ì': 'Trader',
-    'Í': 'Coureurs de Bois',
-    'Ñ': 'Intendant',
-    'Î': 'Fortification (blue)',
-    'Ê': 'Bateaux (huh?)',
-
-    'Ð': 'Home support',
-    'Ï': 'Governor',
-
-    '×': 'Priest',
-
-}
-
-ACTIONS = {
-    # Following the order in game_FewAcresOfSnow
-    0: ('settle', None, ),
-    1: ('develop', None, ),
-    2: ('fortify', None, ),
-    3: ('besiege', None, ),
-    4: ('reinforce siege', location_then_cards, ),
-    5: ('raid', None, ),
-    36: ('raid', None, ),
-    6: ('ambush', None, ),
-    35: ('ambush', None, ),
-    7: ('played Military Leader', None, ),
-    8: ('played Indian Leader/Priest', None, ),
-    9: ('money from', None, ),
-    10: ('merchant', None, ),
-    11: ('trade', None, ),
-    12: ('piracy', None, ),
-    13: ('draft', None, ),
-    14: ('discard', None, ),
-    38: ('discard', None, ),
-    15: ('put into reserve', None, ),
-    16: ('retrieve reserve', None, ),
-    17: ('govern', None, ),
-    18: ('use Intendant', None, ),
-    19: ('home support', None, ),
-    20: ('pass', None, ),
-    21: ('withrdaw from siege', None, ),
-    30: ('win a siege', win_siege, ),
-    35: ('free fur action', None, ),  #special rules
-
-    # From here is legacy junk, to help troubleshoot why I couldn't figure this out before.
-    # '¶': 'successful ambush apparently. First card is protagonist;
-    # third card is the card that was returned to the deck, e.g. Ë =
-    # free French infantry.
-    # the second (T) and fourth (°) are unclear to me.
+    def merchant(detail, which_side):
+        return "Merchant can't yet deal with {}".format(detail)
+        rv = "; ".join([cards_dict[c] if c in cards_dict.keys()
+                           else c for c in detail_code
+                           ])
 
 
-    # '²': 'fortify (UK) or intendant (FR)', None, ),
-    'Â': ('fortify (FR)', None, ),
-    '¶': ('successful ambush [see comments]', None, ),
-    'Î': ('opponent withdrew from siege', None, ),
-}
+    UK_CARDS = [
+        'Military leader',
+        'Regular infantry',
+        'Regular infantry',
+        'Regular infantry',
+        'Regular infantry',
+        'Regular infantry',
+        'Regular infantry',
+        'Indian leader',
+        'Home support',
+        'Trader',
+        'Bateaux',
+        'Siege artillery',
+        'Militia',
+        'Militia',
+        'Militia',
+        'Ships',
+        'Ships',
+        'Settlers',
+        'Rangers',
+        'Fortification (red)',
+        'Governor',
+    ]
 
-LOCATIONS = [
-    "Boston",
-    "New Haven",
-    "New York",
-    "Norfolk",
-    "Pemaquid",
-    "Philadelphia",
-    "St. Mary's",
-    "Albany",
-    "Baltimore",
-    "Canso",
-    "Cumberland",
-    "Deerfield",
-    "Detroit",
-    "Fort Beausejour",
-    "Fort Duquesne",
-    "Fort Frontenac",
-    "Fort Halifax",
-    "Fort Niagara",
-    "Fort Presqu'Isle",
-    "Fort St. John",
-    "Fort Stanwix",
-    "Fort Venango",
-    "Fort William Henry",
-    "Gaspe",
-    "Halifax",
-    "Kennebec",
-    "Louisbourg",
-    "Oswego",
-    "Port Royal",
-    "Richmond",
-    "Tadoussac",
-    "Ticonderoga",
-    "Trois Rivieres",
-    "Montreal",
-    "Quebec",
-    "Michillimackinac",
-]
+    FR_CARDS = {
+        'Ë': 'RegInf (free)',
+        'Ù': 'Regular infantry',
+        'Ú': 'Regular infantry',
+        'Û': 'Regular infantry',
+        'Ò': 'Military leader',
+
+        'Ó': 'Militia',
+        'Ö': 'Native Americans (blue)',
+        'Ì': 'Trader',
+        'Í': 'Coureurs de Bois',
+        'Ñ': 'Intendant',
+        'Î': 'Fortification (blue)',
+        'Ê': 'Bateaux (huh?)',
+
+        'Ð': 'Home support',
+        'Ï': 'Governor',
+
+        '×': 'Priest',
+
+    }
+
+    ACTIONS = {
+        # Following the order in game_FewAcresOfSnow
+        0: ('settle', None,),
+        1: ('develop', None,),
+        2: ('fortify', None,),
+        3: ('besiege', this_class.besiege,),
+        4: ('reinforce siege', location_then_cards,),
+        5: ('raid', None,),
+        36: ('raid', None,),
+        6: ('ambush', None,),
+        35: ('ambush', None,),
+        7: ('played Military Leader', None,),
+        8: ('played Indian Leader/Priest', None,),
+        9: ('money from', None,),
+        10: ('merchant', merchant,),
+        11: ('trade', None,),
+        12: ('piracy', None,),
+        13: ('draft', None,),
+        14: ('discard', None,),
+        38: ('discard', None,),
+        15: ('put into reserve', None,),
+        16: ('retrieve reserve', None,),
+        17: ('govern', None,),
+        18: ('use Intendant', None,),
+        19: ('home support', None,),
+        20: ('pass', None,),
+        21: ('withrdaw from siege', None,),
+        30: ('win a siege', win_siege,),
+        35: ('free fur action', None,),  # special rules
+
+        # From here is legacy junk, to help troubleshoot why I couldn't figure this out before.
+        # '¶': 'successful ambush apparently. First card is protagonist;
+        # third card is the card that was returned to the deck, e.g. Ë =
+        # free French infantry.
+        # the second (T) and fourth (°) are unclear to me.
+
+        # '²': 'fortify (UK) or intendant (FR)', None, ),
+        'Â': ('fortify (FR)', None,),
+        '¶': ('successful ambush [see comments]', None,),
+        'Î': ('opponent withdrew from siege', None,),
+    }
+
+    LOCATIONS = [
+        "Boston",
+        "New Haven",
+        "New York",
+        "Norfolk",
+        "Pemaquid",
+        "Philadelphia",
+        "St. Mary's",
+        "Albany",
+        "Baltimore",
+        "Canso",
+        "Cumberland",
+        "Deerfield",
+        "Detroit",
+        "Fort Beausejour",
+        "Fort Duquesne",
+        "Fort Frontenac",
+        "Fort Halifax",
+        "Fort Niagara",
+        "Fort Presqu'Isle",
+        "Fort St. John",
+        "Fort Stanwix",
+        "Fort Venango",
+        "Fort William Henry",
+        "Gaspe",
+        "Halifax",
+        "Kennebec",
+        "Louisbourg",
+        "Oswego",
+        "Port Royal",
+        "Richmond",
+        "Tadoussac",
+        "Ticonderoga",
+        "Trois Rivieres",
+        "Montreal",
+        "Quebec",
+        "Michillimackinac",
+    ]
+
+    NEUTRAL_CARDS = {
+        'C': 'Native Americans',
+        'D': 'Native Americans',
+        'E': 'Native Americans',
+        'F': 'Native Americans',
+        'G': 'Native Americans',
+        'H': 'Settlers',
+        'I': 'Settlers',
+    }
 
 
 
-def action_code_to_action(code, which_side):
-    ## TODO - extract out the simplest handler, what I'm calling cards here,
-    # to be a default handler.
-    # action = ACTIONS[code[0]] if code[0] in ACTIONS.keys() else code[0]
-    char_code0 = ord(code[0]) - 176
-    if char_code0 in ACTIONS.keys():
-        action, handler = ACTIONS[char_code0]
-    else:
-        action = code[0] + " -- " + str(char_code0)
-        handler = None
-    return "{action}: {detail}".format(
-        action=action, detail=(handler(code[1:]) if handler else
-                               simple_cards(code[1:], which_side)))
+    def action_code_to_action(self, code):
+        ## TODO - extract out the simplest handler, what I'm calling cards here,
+        # to be a default handler.
+        # action = ACTIONS[code[0]] if code[0] in ACTIONS.keys() else code[0]
+        char_code0 = ord(code[0]) - 176
+        if char_code0 in self.ACTIONS.keys():
+            action, handler = self.ACTIONS[char_code0]
+        else:
+            action = code[0] + " -- " + str(char_code0)
+            handler = None
+        return "{action}: {detail}".format(
+            action=action, detail=(handler(code[1:]) if handler else
+                                   self.simple_cards(code[1:])))
 
-def move_to_actions(move, which_side):
-    list_of_action_codes = move.split(',')
-    return [action_code_to_action(ac, which_side) for ac in list_of_action_codes]
+    def move_to_actions(self, move):
+        list_of_action_codes = move.split(',')
+        return [self.action_code_to_action(ac) for ac in list_of_action_codes]
 
-def file_to_history(filename):
-    pass
+    def iterate_through_moves(self):
+        for move in self.moves_list:
+            self.which_side = ['uk', 'fr'][self.move_number % 2]
+            self.actions_list.append(self.move_to_actions(move))
+            self.move_number += 1
+        return self.moves_list
+
+
+    def file_to_history(filename):
+        pass
+
 
 def main():
     pass
@@ -305,16 +300,19 @@ def test1():
     # print(["****" + ms for )
 
 def test2():
-    players = ['uk', 'fr']
-    i = 0
+    analyzer = FewAcresOfSnowAnalyzer(moves9575653_fr)
+    pprint.pprint(analyzer.iterate_through_moves())
+    # players = ['uk', 'fr']
+    # i = 0
 
-    for test in moves9575653_fr[:20]:
-        current = i % 2
-        print("{}: move {}".format(players[current], i + 1))
-        for action in move_to_actions(test, players[current]):
-            print("-- {}".format(action))
-        print()
-        i += 1
+
+    # for test in moves9575653_fr[:20]:
+    #     current = i % 2
+    #     print("{}: move {}".format(players[current], i + 1))
+    #     for action in analyzer.move_to_actions(test):
+    #         print("-- {}".format(action))
+    #     print()
+    #     i += 1
 
 
 if __name__ == "__main__":
@@ -332,3 +330,26 @@ HistoryStatus[1] =
     }
 x='¿'; moo();
 """
+
+OLD_UK_CARDS = {
+
+        'Ó': 'Regular infantry',
+        'Õ': 'Regular infantry',
+        'Ö': 'Regular infantry',
+        'Ò': 'Regular infantry',
+        'Ô': 'Regular infantry',
+        'ã': 'Rangers',
+
+
+
+
+        'Ø': 'Indian leader',
+        'Ñ': 'Military leader',
+        'Ý': 'Militia',
+        'Þ': 'Militia',
+        'å': 'Governor',
+        'ä': 'Fortification (red)',
+
+        'Ù': 'Home Support',
+
+    }
