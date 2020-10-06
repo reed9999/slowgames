8de75be792b4b2d0994726f39f1a7d37f3402d71
Reed, Philip <Philip.Reed@anthem.com>
2020-10-05 21:26:35 -0400

    PASS only for test_not_much. start to shows the way forward in bs4 parse

--- concordia/tests/test_concordia_parsing.py
+++ concordia/tests/test_concordia_parsing.py
@@ -20,12 +20,53 @@ class TestConcordiaParsing(object):
 
     @pytest.mark.parametrize('filename',
                              ['game-ccd55950.html'])
+    def test_read_a_line(self, filename):
+        full_fn = os.path.join(THIS_FILE_DIR, '..', 'games', filename)
+        with open(full_fn, 'r') as f:
+            lines = f.readlines()
+        history = ConcordiaHistory(filename=full_fn)
+        print(lines[0:20])
+        assert history.move_from_line(lines[17]) is not None
+
+    @pytest.mark.parametrize('filename',
+                             ['game-ccd55950.html'])
     def test_not_much(self, filename):
+        # Leave this until we have a BS4 Quickstart-based test.
         full_fn = os.path.join(THIS_FILE_DIR, '..', 'games', filename)
         with open(full_fn, 'r') as f:
             soup = BeautifulSoup(f.read(), 'html.parser')
-        assert soup is not None
-        print(soup)
+        history_table = soup.find_all('table', 'clHisto')
+        print([str(x)[0:100] for x in history_table])
+        assert len(history_table) > 0
+        assert len(history_table) == 1
+
+        all_th = [x.text for x in history_table[0].find_all('th')]
+        all_td = [x.text for x in history_table[0].find_all('td')]
+        assert all_th == ['', 'philip9999', 'Atroniclus']
+        assert all_td[:10] == [
+            '2020-08-29 at 20:05',
+            'End of game - Scoring\n'
+            '\n'
+            '\n'
+            'philip9999Atroniclus07114424334420168249001800120200147134',
+            '',
+            '0',
+            '7',
+            '',
+            '1',
+            '1',
+            '',
+            '44',
+        ]
+        assert all_td[50:55] == [
+            'Move colonists',
+            'Atroniclus2020-08-29 at 16:01',
+            'Plays the card\xa0',
+            'philip99992020-08-29 at 13:13',
+            'Buys\xa0:\xa01\xa0Cost\xa0:\xa05'
+              'Buys\xa0:\xa01\xa0Cost\xa0:\xa03',
+    ]
+
 
 def main():
     pass
