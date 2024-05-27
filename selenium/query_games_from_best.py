49a438f0cc75ec4c6923e0077d01085bb6ab1fc1
Philip J. Reed <reed9999>
2024-05-27 16:12:09 -0400

    accept one game per run for now
    Eventually will want to iterate once we organize the output.

--- selenium/query_games_from_best.py
+++ selenium/query_games_from_best.py
@@ -28,7 +28,41 @@ GAME_URL_NAMES = [
         "ClaimIt",
         ]
 
-STOP_SHORT = 2
+"""
+<select id="selFilterGameType"><  
+"-1">All games</option><  
+"301">7 Steps</option><  
+"90">A Few Acres of Snow</option><  
+"97">At the Gates of Loyang</option><  
+"38">Atoll</option><  
+"380">Bonfire</option><  
+"321">Bruges</option><  
+"63">Campaign Manager 2008</option><  
+"85">Can't Stop</option><  
+"98">Carson City</option><  
+"332">Claim It</option><  
+"372">Fields of Arle</option><  
+"327">Ground Floor</option><  
+"355">Hadara</option><  
+"108">Jaipur</option><  
+"20">Just 4 Fun</option><  
+"56">Just 4 Fun Colours</option><  
+"313">La Granja</option><  
+"376">Newton</option><  
+"366">On the Underground</option><  
+"323">Rajas of the Ganges</option><  
+"142">Russian Railroads</option><  
+"359">Saint Petersburg</option><  
+"405">Skymines</option><  
+"309">Snowdonia</option><  
+"345">Steamrollers</option><  
+"49">Stone Age</option><  
+"126">The Castles of Burgundy</option><  
+"324">The Oracle of Delphi</option><  
+"347">Transatlantic</option></select>
+"""
+
+
 
 def main():
     driver = webdriver.Chrome()
@@ -41,12 +75,15 @@ def main():
     log_in_to_yucata(driver, uname, pw)
     
     top_players_dict = {}
-    for game in ["SaintPetersburg2"]: ### GAME_URL_NAMES:
+    # For now only put one game in this list to avoid mixing output files.
+    # We can consider more than one when we start sorting the output correctly
+    current_games_considered = ["CarsonCity", ]
+    for game in current_games_considered:
         players = get_top_players(driver, game)
         top_players_dict[game] = players
 
     game_game_ids_dict = {}
-    for game in ["SaintPetersburg2", ]:
+    for game in current_games_considered:
         game_game_ids_dict[game] = []
         for player in top_players_dict[game]:
             pp(f"Games for player {player} game {game}")
@@ -54,11 +91,9 @@ def main():
             pp(f"for player {player} len {len(gg)}")
             game_game_ids_dict[game] += gg
 
-    pp(game_game_ids_dict.keys())
-    pp(game_game_ids_dict["SaintPetersburg2"][:10])
     # map(lambda x: get_and_save_game(driver, x), game_game_ids_dict["SaintPetersburg2"])
-    for gid in game_game_ids_dict["SaintPetersburg2"]:
-        get_and_save_game(driver, gid)
+    for gid in game_game_ids_dict[current_games_considered[0]]:
+        get_and_save_game(driver, gid, )
 
 
 def log_in_to_yucata(driver, username: str, password: str):
@@ -94,7 +129,10 @@ def get_top_players(driver, game: str) -> list[str] | None:
 def get_player_games(driver, player, game):
     ids = {
         "Atoll": 38,
+        "CarsonCity": 98,
+        "Rajas": 323,
         "SaintPetersburg2": 359,
+        "Transatlantic": 347,
         }
     game_id = ids[game]
     driver.get(f"https://www.yucata.de/en/Ranking/{player}#game_{game_id}")
